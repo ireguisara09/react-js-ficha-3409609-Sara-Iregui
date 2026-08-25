843fdd8d765b721f336fc2fdc1e6b80012419e51
Sara Lucia Meneces Iregui <ireguisara.sena09@gmail.com>
2026-08-25 09:56:00 -0500

    Agregar eventos a ProductoCard

---
+++ tienda-react/src/components/ProductoCard.jsx
@@ -0,0 +1,36 @@
+function ProductoCard({ producto }) {
+const {
+nombre,
+
+precio,
+categoria,
+stock
+} = producto;
+const estado =
+stock > 0
+? "Disponible"
+: "Agotado";
+const mostrarProducto = () => {
+alert(`Seleccionaste ${nombre}`);
+};
+return (
+<article className="producto-card">
+<h2>{nombre}</h2>
+<p>Categoría: {categoria}</p>
+<p>Precio: ${precio}</p>
+<p>Stock: {stock}</p>
+<strong>{estado}</strong>
+<br />
+<button
+onClick={mostrarProducto}
+disabled={stock === 0}
+>
+{
+stock > 0 ? "Ver producto" : "Agotado"
+}
+</button>
+</article>
+);
+}
+
+export default ProductoCard;
