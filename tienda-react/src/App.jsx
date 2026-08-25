f76f33d8b198eb0ed8ad26872d8b63e492b5c5d1
Sara Lucia Meneces Iregui <ireguisara.sena09@gmail.com>
2026-08-25 10:10:26 -0500

    Implementar buscador de productos

--- tienda-react/src/App.jsx
+++ tienda-react/src/App.jsx
@@ -1,6 +1,7 @@
 import ProductoCard from './components/ProductoCard'; 
 import { productos } from './data/productos'; 
 import './App.css'; 
+import { useState } from "react";
 function App() { 
  const disponibles = productos.filter(producto => producto.stock > 0); 
  const hayAgotados = productos.some(producto => producto.stock === 0);
@@ -8,19 +9,42 @@ function App() {
  (total, producto) => total + producto.precio * producto.stock, 
  0 
  ); 
+ const [busqueda, setBusqueda] = useState("");
+ const productosFiltrados =
+productos.filter(producto =>
+producto.nombre
+.toLowerCase()
+.includes(
+busqueda.toLowerCase()
+)
+);
  return ( 
  <main className="contenedor"> 
  <h1>Tienda tecnológica</h1> 
  <p>Productos disponibles: {disponibles.length}</p>
  <p>Productos agotados: {hayAgotados.toString()}</p> 
  <p>Valor total del inventario: ${valorInventario}</p> 
+ <input
+  type="text"
+  placeholder="Buscar producto..."
+  value={busqueda}
+  onChange={(evento) => {
+  setBusqueda(evento.target.value);
+}}
+/>
+<br>
+</br>
+<br>
+</br> 
+
  <section className="productos"> 
- {productos.map(producto => ( 
- <ProductoCard 
- key={producto.id} 
- producto={producto} 
- /> 
- ))} 
+ {productosFiltrados.map(producto => (
+<ProductoCard
+key={producto.id}
+producto={producto}
+/>
+))}
+
  </section> 
  </main> 
  ); 
