209cd405ad7185253abb4992b568fbf26259048e
Sara Lucia Meneces Iregui <ireguisara.sena09@gmail.com>
2026-08-31 09:05:09 -0500

    Creación de la función manejarCambio

--- tienda-react/src/components/FormularioProducto.jsx
+++ tienda-react/src/components/FormularioProducto.jsx
@@ -6,6 +6,12 @@ categoria: "",
 precio: "",
 stock: ""
 });
+const manejarCambio = (evento) => {
+setFormulario({
+...formulario,
+[evento.target.name]: evento.target.value
+});
+};
 return (
 <form>
 <h2>Agregar producto</h2>
@@ -13,21 +19,29 @@ return (
 type="text"
 name="nombre"
 placeholder="Nombre"
+value={formulario.nombre}
+onChange={manejarCambio}
 />
 <input
 type="text"
 name="categoria"
 placeholder="Categoría"
+value={formulario.categoria}
+onChange={manejarCambio}
 />
 <input
 type="number"
 name="precio"
 placeholder="Precio"
+value={formulario.precio}
+onChange={manejarCambio}
 />
 <input
 type="number"
 name="stock"
 placeholder="Stock"
+value={formulario.stock}
+onChange={manejarCambio}
 />
 <button type="submit">
 Agregar producto
