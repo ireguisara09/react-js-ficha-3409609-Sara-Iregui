34e3b84c18f347109b6dbde07cde9bb7e6fcf167
aleediazc0208-afk <aleediazc0208@gmail.com>
2026-09-07 10:13:10 -0500

    feat: Mision 5 : correcion en Nuevoproducto

--- tienda-react/src/pages/NuevoProducto.jsx
+++ tienda-react/src/pages/NuevoProducto.jsx
@@ -1,12 +1,20 @@
 // pages/NuevoProducto.jsx
+import { useNavigate } from "react-router";
 import FormularioProducto from "../components/FormularioProducto";
 
 function NuevoProducto({ agregarProducto }) {
+  const navigate = useNavigate();
+
+  const manejarAgregar = (nuevoProducto) => {
+    agregarProducto(nuevoProducto);
+    navigate("/inventario");
+  };
+
   return (
     <section>
       <h1>Nuevo producto</h1>
 
-      <FormularioProducto onAgregar={agregarProducto} />
+      <FormularioProducto onAgregar={manejarAgregar} />
     </section>
   );
 }
